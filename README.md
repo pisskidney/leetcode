9f7160109420a23c3042f52c5a7b7c424f9621a4
Peter Horvath <horvthpeter@gmail.com>
2021-11-03 18:13:19 +0200

    Cool permutations solution.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 X
 =
 
-* (40) Combination Sum II https://leetcode.com/problems/combination-sum-ii/
+* (40) Combination Sum II - https://leetcode.com/problems/combination-sum-ii/
+* (46) Permutations - https://leetcode.com/problems/permutations/
--- medium/46.py
+++ medium/46.py
@@ -1,26 +1,35 @@
-# Given a collection of distinct integers, return all possible permutations.
-
-
 from typing import List
+
+
+"""
+46. Permutations
+
+https://leetcode.com/problems/permutations/
+"""
 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        numss = set(nums)
-        def perm(numbers, current):
-            if len(current) == len(numbers):
-                res.append(current[:])
-                return current
-            for n in numbers:
-                if n not in current:
-                    current.append(n)
-                    perm(numbers, current)
-                    current.pop()
-        perm(nums, [])
+
+        def p(numbers, left, right):
+            if left == right:
+                res.append(numbers[:])
+                return
+            for i in range(left, right + 1):
+                numbers[left], numbers[i] = numbers[i], numbers[left]
+                p(numbers, left + 1, right)
+                numbers[left], numbers[i] = numbers[i], numbers[left]
+        p(nums, 0, len(nums) - 1)
+
         return res
 
 
-s = Solution()
-a = [1, 2, 3]
-print(s.permute(a))
+def main():
+    sol = Solution()
+    print(sol.permute([1, 2, 3]))
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
