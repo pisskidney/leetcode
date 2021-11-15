2a3ee85c561b91ad79663e7085ff53202097f7d0
Peter Horvath <horvthpeter@gmail.com>
2021-11-15 14:15:27 +0200

    2 Solutions for superset.

--- medium/78.py
+++ medium/78.py
@@ -14,6 +14,26 @@ class Solution:
             if left >= len(nums):
                 res.append(current[:])
                 return
+            for i in range(left, len(nums) + 1):
+                if i < len(nums):
+                    current.append(nums[i])
+                    go(i + 1, current)
+                    current.pop()
+                else:
+                    go(i + 1, current)
+
+        go(0, [])
+        return res
+
+
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def go(left, current):
+            if left >= len(nums):
+                res.append(current[:])
+                return
             current.append(nums[left])
             go(left + 1, current)
             current.pop()
@@ -26,6 +46,8 @@ class Solution:
 def main():
     sol = Solution()
     print(sol.subsets([1, 2, 3]))
+    print(sol.subsets([0]))
+    print(sol.subsets([0, 1]))
     return 0
 
 
