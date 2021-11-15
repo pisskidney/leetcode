d1e4462006bd43c56b173467612469009f96056b
Peter Horvath <horvthpeter@gmail.com>
2021-11-15 12:11:50 +0200

    Simple problems practice.

--- medium/73.py
+++ medium/73.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python
+from typing import List
 
 
-class Solution(object):
-    def setZeroes(self, matrix):
+"""
+73. Set Matrix Zeroes
+
+https://leetcode.com/problems/set-matrix-zeroes/
+"""
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: void Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        firstCol = False
-        for i in xrange(len(matrix)):
-            if matrix[i][0] == 0:
-                firstCol = True
-                break
-        for i in xrange(len(matrix)):
-            for j in xrange(1, len(matrix[0])):
+        rows, cols = set([]), set([])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    matrix[i][0] = 0
-        for i in xrange(len(matrix)-1, -1, -1):
-            for j in xrange(1, len(matrix[0])):
-                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in rows or j in cols:
                     matrix[i][j] = 0
-        if firstCol:
-            for i in xrange(len(matrix)):
-                matrix[i][0] = 0
 
 
-s = Solution()
-a = [[1, 2, 3], [0, 2, 5], [6, 9, 0]]
-print a
-s.setZeroes(a)
-print a
+def main():
+    sol = Solution()
+    a = [1, 1, 1], [1, 0, 1], [1, 1, 1]
+    print(sol.setZeroes(a))
+    print(a)
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
--- medium/74.py
+++ medium/74.py
@@ -1,37 +1,36 @@
-#!/usr/bin/python
+"""
+74. Search a 2D Matrix
+
+https://leetcode.com/problems/search-a-2d-matrix/
+"""
+from typing import List
 
 
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        if not matrix or not matrix[0]:
-            return False
-        i, j = 0, len(matrix) * len(matrix[0]) - 1
-        while i <= j:
-            mid = (i + j) / 2
-            row = mid / len(matrix[0])
-            col = mid % len(matrix[0])
-            if matrix[row][col] == target:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n, m = len(matrix), len(matrix[0])
+        i = j = 0
+        while i < n and j < m and matrix[i][j] <= target:
+            if matrix[i][j] == target:
                 return True
-            elif matrix[row][col] < target:
-                i = mid + 1
+            elif (i + 1 < n and matrix[i+1][j] <= target):
+                i += 1
             else:
-                j = mid - 1
+                j += 1
         return False
-        
-
-s = Solution()
-print s.searchMatrix(
-    [
-    [1,   3,  5,  7],
-    [10, 11, 16, 20],
-    [23, 30, 34, 50]
-    ],
-    50
-)
 
 
+def main():
+    sol = Solution()
+    print(sol.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
+    print(sol.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+    print(sol.searchMatrix([[1]], 2))
+    print(sol.searchMatrix([[1]], 1))
+    print(sol.searchMatrix([[1]], 0))
+    print(sol.searchMatrix([[1], [2], [3], [5]], 4))
+    print(sol.searchMatrix([[1], [2], [3], [5]], 5))
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
