1a30624fd1c0aca11a7ce755a3353f1019b70030
Peter Horvath <horvthpeter@gmail.com>
2021-11-18 11:06:49 +0200

    LinkedList problem, clean solution.

--- medium/82.py
+++ medium/82.py
@@ -3,6 +3,7 @@
 
 https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
 """
+from typing import Optional
 
 
 # Definition for singly-linked list.
@@ -12,27 +13,51 @@ class ListNode:
         self.next = next
 
 
+def printx(head: Optional[ListNode]) -> None:
+    p = head
+    res = []
+    while p is not None:
+        res.append(p.val)
+        p = p.next
+    print(res)
+
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         p = head
+        prev = None
         root = ListNode()
         q = root
-        avoid = None
         while p is not None:
-            if p.val == avoid:
-                p = p.next
-                continue
-            if p.next and p.next.val == p.val:
-                avoid = p.val
-                p = p.next
-                continue
-            q.next = p
-            q = q.next
+            if (p.next is None or p.next.val != p.val) and (prev is None or prev.val != p.val):
+                q.next = p
+                q = q.next
+            prev = p
+            p = p.next
+        q.next = None
+        return root.next
 
 
 def main():
     s = Solution()
-    print(s.xxx())
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    printx(head)
+    printx(s.deleteDuplicates(head))
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(5)))))
+    printx(head)
+    printx(s.deleteDuplicates(head))
+    head = ListNode(2, ListNode(2, ListNode(3, ListNode(3, ListNode(5)))))
+    printx(head)
+    printx(s.deleteDuplicates(head))
+    head = ListNode(2, ListNode(2, ListNode(3, ListNode(3, ListNode(5, ListNode(5))))))
+    printx(head)
+    printx(s.deleteDuplicates(head))
+    head = ListNode(2)
+    printx(head)
+    printx(s.deleteDuplicates(head))
+    head = None
+    printx(head)
+    printx(s.deleteDuplicates(head))
 
 
 if __name__ == '__main__':
