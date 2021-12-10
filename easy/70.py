0636bde240f613a2a329a5f7e7d4db14e5799a10
Peter Horvath <horvthpeter@gmail.com>
2021-12-10 10:15:50 +0200

    Famous little proby.

--- easy/70.py
+++ easy/70.py
@@ -1,9 +1,26 @@
-#!/usr/bin/python
+"""
+70. Climbing Stairs
+
+https://leetcode.com/problems/climbing-stairs/
+"""
 
 
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    visited = {}
+
+    def climbStairs(self, n: int) -> int:
+        if n in self.visited:
+            return self.visited[n]
+        if n <= 1:
+            return 1
+        self.visited[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.visited[n]
+
+
+def main():
+    s = Solution()
+    print(s.climbStairs(45))
+
+
+if __name__ == '__main__':
+    raise(SystemExit(main()))
