ba64a0485107df864013503a4a305fb26ad24b1a
Peter Horvath <horvthpeter@gmail.com>
2022-02-11 00:05:08 +0200

    Finish this nice little problem.

--- medium/560.py
+++ medium/560.py
@@ -11,14 +11,12 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
         summ = 0
-        cumsum = []
         h = defaultdict(int)
+        h[0] = 1
         for num in nums:
             summ += num
-            cumsum.append(summ)
-        for num in cumsum:
-            res += h[num-k]
-            h[num] += 1
+            res += h[summ-k]
+            h[summ] += 1
         return res
 
 
